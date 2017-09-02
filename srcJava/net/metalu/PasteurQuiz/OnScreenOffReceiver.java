3730358e0240876d79911a111b3d670d8860324d
Antoine Rousseau <_antoine_@metalu.net>
2017-09-02 16:20:19 +0200

    don't lock screen when charging

--- srcJava/net/metalu/PasteurQuiz/OnScreenOffReceiver.java
+++ srcJava/net/metalu/PasteurQuiz/OnScreenOffReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
+import android.os.BatteryManager;
+import android.content.IntentFilter;
 
 public class OnScreenOffReceiver extends BroadcastReceiver {
 
@@ -11,8 +13,19 @@ public class OnScreenOffReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if(Intent.ACTION_SCREEN_OFF.equals(intent.getAction())){
             AppContext ctx = (AppContext) context.getApplicationContext();
+            
+            IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+			Intent batteryStatus = context.registerReceiver(null, ifilter);
+			int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+			boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
+
+			// How are we charging?
+			int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+			boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+			boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
             // is Kiosk Mode active?
-            if(PrefUtils.isKioskModeActive(ctx)) {
+            if(PrefUtils.isKioskModeActive(ctx) && (chargePlug == 0)) {
                 wakeUpDevice(ctx);
             }
         }
